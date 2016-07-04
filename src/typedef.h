3802779cc747f142bae9820a907d8bd2651f42ba
Drew Schmidt <wrathematics@gmail.com>
2016-07-04 18:45:59 -0400

    use alias templates

--- src/typedef.h
+++ src/typedef.h
@@ -1,17 +1,12 @@
+#ifndef TRIEBEARD_TYPEDEF_H_
+#define TRIEBEARD_TYPEDEF_H_
+
 #include <radix.h>
 
-void finaliseRadixStr(radix_tree <std::string, std::string>* radix_inst){}
+template <typename T>
+void finaliseRadix(radix_tree <std::string, T>* radix_inst){}
 
-typedef Rcpp::XPtr<radix_tree <std::string, std::string>, Rcpp::PreserveStorage, finaliseRadixStr> XPtrRadixStr;
+template <typename T>
+using XPtrRadix = Rcpp::XPtr<radix_tree <std::string, T>, Rcpp::PreserveStorage, finaliseRadix<T> >;
 
-void finaliseRadixInt(radix_tree <std::string, int>* radix_inst){}
-
-typedef Rcpp::XPtr<radix_tree <std::string, int>, Rcpp::PreserveStorage, finaliseRadixInt> XPtrRadixInt;
-
-void finaliseRadixBool(radix_tree <std::string, bool>* radix_inst){}
-
-typedef Rcpp::XPtr<radix_tree <std::string, bool>, Rcpp::PreserveStorage, finaliseRadixBool> XPtrRadixBool;
-
-void finaliseRadixDouble(radix_tree <std::string, double>* radix_inst){}
-
-typedef Rcpp::XPtr<radix_tree <std::string, double>, Rcpp::PreserveStorage, finaliseRadixDouble> XPtrRadixDouble;
+#endif
